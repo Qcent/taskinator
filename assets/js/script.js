8b2414b3c977a079a05972b22fda34851a1d4736
Dave Quinn <dquinn8@cogeco.ca>
2021-09-07 12:20:26 -0400

    updated form submission event handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 let buttonEl = document.querySelector("#save-task");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
+let formEl = document.querySelector("#task-form");
 
-const createTaskHandler = function() {
+const createTaskHandler = function(event) {
+    event.preventDefault();
     let taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "newTask";
     tasksToDoEl.appendChild(taskItemEl);
+
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+// buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
