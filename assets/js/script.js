ecd4783029cc29b848e59e50f98b41ec0546deec
Dave Quinn <dquinn8@cogeco.ca>
2021-09-07 13:41:12 -0400

    refactored form submission logic / added validation and form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,37 @@
-let buttonEl = document.querySelector("#save-task");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 let formEl = document.querySelector("#task-form");
 
 
-const createTaskHandler = function(event) {
+const taskFormHandler = function(event) {
     event.preventDefault(); // stop page refresh on submit
 
-    // ge the name and type of task to bbe added from the user submitted form
+    // get the name and type of task to bbe added from the user submitted form
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    /** Validate inputs  */
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false; // kill the rest of code exicution if invalid input detected
+    }
+    /** if valid continue */
+
+    formEl.reset(); //reset the form values
+    // package as an object with properties {name, type} to be sent to createTaskEl()
+    createTaskEl({
+        name: taskNameInput,
+        type: taskTypeInput
+    });
+}
+
+const createTaskEl = function(task) {
     let taskItemEl = document.createElement("li"); //create a new task item
     taskItemEl.className = "task-item"; //give it a class name
 
     let taskInfoEl = document.createElement("div"); //create a taks info container
     taskInfoEl.className = "task-info"; // set its class property
-
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + // task name
-        "</h3><span class='task-type'>" + taskTypeInput + "</span>"; // & type
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + task.name + // task name
+        "</h3><span class='task-type'>" + task.type + "</span>"; // & type
 
     taskItemEl.appendChild(taskInfoEl); // append the info to the item
 
@@ -25,4 +39,4 @@ const createTaskHandler = function(event) {
 }
 
 // buttonEl.addEventListener("click", createTaskHandler);
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
