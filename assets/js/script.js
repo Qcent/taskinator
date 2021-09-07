52b600e8174b8f12a2ad1fdbd8f8604495d85b3b
Dave Quinn <dquinn8@cogeco.ca>
2021-09-07 12:43:32 -0400

    form dadta is now captured and added in the form of a new task in list

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,26 @@ let buttonEl = document.querySelector("#save-task");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 let formEl = document.querySelector("#task-form");
 
-const createTaskHandler = function(event) {
-    event.preventDefault();
-    let taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = "newTask";
-    tasksToDoEl.appendChild(taskItemEl);
 
+const createTaskHandler = function(event) {
+    event.preventDefault(); // stop page refresh on submit
+
+    // ge the name and type of task to bbe added from the user submitted form
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    let taskItemEl = document.createElement("li"); //create a new task item
+    taskItemEl.className = "task-item"; //give it a class name
+
+    let taskInfoEl = document.createElement("div"); //create a taks info container
+    taskInfoEl.className = "task-info"; // set its class property
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + // task name
+        "</h3><span class='task-type'>" + taskTypeInput + "</span>"; // & type
+
+    taskItemEl.appendChild(taskInfoEl); // append the info to the item
+
+    tasksToDoEl.appendChild(taskItemEl); // append the item to the task list
 }
 
 // buttonEl.addEventListener("click", createTaskHandler);
